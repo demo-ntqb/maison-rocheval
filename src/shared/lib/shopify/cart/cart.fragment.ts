547b91fe8f990a1fe86bd66f0abeca0750fa9e12
demo-ntqb <ytbntq001@gmail.com>
2026-08-29 17:43:51 +0700

    feat(cart): add shared cart fragment

---
+++ src/shared/lib/shopify/cart/cart.fragment.ts
@@ -0,0 +1,34 @@
+export const MAISON_CART_FRAGMENT = `#graphql
+  fragment MaisonCart on Cart {
+    id
+    totalQuantity
+    checkoutUrl
+    buyerIdentity { countryCode }
+    cost {
+      subtotalAmount { amount currencyCode }
+    }
+    lines(first: $first, after: $after) {
+      pageInfo { hasNextPage endCursor }
+      nodes {
+        id
+        quantity
+        attributes { key value }
+        cost {
+          amountPerQuantity { amount currencyCode }
+          subtotalAmount { amount currencyCode }
+        }
+        merchandise {
+          ... on ProductVariant {
+            id
+            title
+            availableForSale
+            quantityAvailable
+            selectedOptions { name value }
+            image { url altText width height }
+            product { id handle title productType }
+          }
+        }
+      }
+    }
+  }
+` as const;
